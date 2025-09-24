18b8124fe52daf73e05e34ac14e0e8e8f3c21c6f
Gonzzzaa <gonzalopereyra001@gmail.com>
2025-09-24 18:51:36 -0300

    update

--- src/modules/teams/teams.controller.ts
+++ src/modules/teams/teams.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Get, Put, Delete, Param, Body } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { CreateTeamDto } from './dto/create-team.dto';
+import { UpdateTeamDto } from './dto/update-team.dto';
+
 
 @Controller('teams')
 export class TeamsController {
@@ -15,7 +17,17 @@ export class TeamsController {
   findAll() {
     return this.teamsService.findAll();
   }
+  
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.teamsService.findOne(+id);
+  }
 
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTeamDto) {
+    return this.teamsService.update(+id, dto);
+  }
+  
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.teamsService.remove(+id);
