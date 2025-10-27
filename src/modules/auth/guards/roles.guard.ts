d358ed9b4a88550b2504c2fd2175e67c406db449
Alekk73 <sntxdev@gmail.com>
2025-10-27 10:07:37 -0300

    Roles Guard

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { ROLES_KEY } from 'src/common/decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext) {
+    const requiredRoles = this.reflector.getAllAndOverride(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest<Request>();
+
+    if (user.rol !== requiredRoles) {
+      throw new UnauthorizedException('No tienes rol para ejecutar la acción');
+    }
+
+    return true;
+  }
+}
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get, Put, Param, Body, ParseIntPipe, HttpStatus, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Param,
+  Body,
+  ParseIntPipe,
+  HttpStatus,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RolesUser } from 'src/common/enums/roles-user.enum';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('users')
 export class UsersController {
@@ -19,5 +32,12 @@ export class UsersController {
     @Body() updateUserDto: UpdateUserDto,
   ) {
     return await this.usersService.update(id, updateUserDto);
+  }
+
+  @Get('change-captain')
+  @Roles(RolesUser.capitan)
+  @UseGuards(RolesGuard)
+  async changeCaptain() {
+    return this.usersService.changeCaptain();
   }
 }
