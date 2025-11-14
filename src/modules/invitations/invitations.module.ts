100eb4cf0acc9229ebd93245ac542ccdb1522b9c
Alekk73 <sntxdev@gmail.com>
2025-11-14 00:35:37 -0300

    feat(invitations): se agrega funcionalidad para crear invitaciones
    - Estructura de respuesta

--- src/modules/invitations/invitations.module.ts
+++ src/modules/invitations/invitations.module.ts
@@ -3,9 +3,15 @@ import { InvitationsService } from './invitations.service';
 import { InvitationsController } from './invitations.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvitationEntity } from './entities/invitation.entity';
+import { UsersModule } from '../users/users.module';
+import { TeamsModule } from '../teams/teams.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([InvitationEntity])],
+  imports: [
+    TypeOrmModule.forFeature([InvitationEntity]),
+    UsersModule,
+    TeamsModule,
+  ],
   controllers: [InvitationsController],
   providers: [InvitationsService],
 })
