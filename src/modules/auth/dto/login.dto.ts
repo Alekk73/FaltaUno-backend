7532a1a3beb9a4aa30811b960b9cdf0a39b52aab
Alexis Allende <86061460+Alekk73@users.noreply.github.com>
2025-11-22 22:42:49 -0300

    Inicio de documentación
    - Auth module

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
 import { Transform } from 'class-transformer';
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ example: 'juan@mail.com' })
   @IsEmail()
   correo_electronico: string;
 
+  @ApiProperty({ example: '123456' })
   @IsString()
   @MinLength(6)
   @Transform(({ value }) => value.trim())
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,18 +1,23 @@
 import { Transform } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'Juan' })
   @IsNotEmpty()
   @IsString()
   nombre: string;
 
+  @ApiProperty({ example: 'Pérez' })
   @IsNotEmpty()
   @IsString()
   apellido: string;
 
+  @ApiProperty({ example: 'juan@mail.com' })
   @IsEmail()
   correo_electronico: string;
 
+  @ApiProperty({ example: '123456' })
   @IsString()
   @MinLength(6)
   @Transform(({ value }) => value.trim())
