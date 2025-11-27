95c695fdd963361e649e1b9b1f447fcfad80cabe
Gonzzzaa <gonzalopereyra001@gmail.com>
2025-11-27 06:14:23 -0300

    error en el pulrequestl

--- src/modules/matches/entities/match-team.entity.ts
+++ src/modules/matches/entities/match-team.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from 'src/common/base.entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { MatchEntity } from './match.entity';
-import { TeamEntity } from 'src/modules/teams/entity/team.entity';
+import { TeamEntity } from 'src/modules/teams/entities/team.entity';
 
 @Entity({ name: 'partidos_equipos' })
 export class MatchTeamEntity extends BaseEntity {
