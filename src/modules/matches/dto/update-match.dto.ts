8519d86819a39676681ea51c2b033488634bf30c
Alekk73 <sntxdev@gmail.com>
2025-11-27 20:10:33 -0300

    refactor(matches) - joinMatch ajustado - create, ahora se puede crear partido sin contrincante, para que se pueda unir otro equipo
    feat(matches)
    - changeResult
    - confirmResult

--- src/modules/matches/dto/update-match.dto.ts
+++ src/modules/matches/dto/update-match.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMatchDto } from './create-match.dto';
+import { IsNumber } from 'class-validator';
 
-export class UpdateMatchDto extends PartialType(CreateMatchDto) {}
+export class UpdateMatchDto extends PartialType(CreateMatchDto) {
+  @IsNumber()
+  goles_local?: number;
+
+  @IsNumber()
+  goles_visitante?: number;
+}
