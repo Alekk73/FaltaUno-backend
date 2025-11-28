d8bf1b18c9c81c809dec60581783fd7fe3567616
Alekk73 <sntxdev@gmail.com>
2025-11-28 10:05:02 -0300

    Documentación User

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum, IsOptional, IsString, Length } from 'class-validator';
 import { RolesUser } from 'src/common/enums/roles-user.enum';
 
 export class UpdateUserDto {
+  @ApiProperty({ example: 'Pepe' })
   @IsOptional()
   @IsString()
   @Length(1, 50)
   nombre?: string;
 
+  @ApiProperty({ example: 'Argento' })
   @IsOptional()
   @IsString()
   @Length(1, 50)
   apellido?: string;
 
+  @ApiProperty({ example: 'pepeargento@mail.com' })
   @IsOptional()
   @IsEmail()
   correo_electronico?: string;
