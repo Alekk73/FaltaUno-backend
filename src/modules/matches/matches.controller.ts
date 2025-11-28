c4fba5862466970c9a68ce82cfa006aeffbf9078
Alexis Allende <86061460+Alekk73@users.noreply.github.com>
2025-11-28 11:11:17 -0300

    Documentación Matches

--- src/modules/matches/matches.controller.ts
+++ src/modules/matches/matches.controller.ts
@@ -18,26 +18,76 @@ import { UpdateMatchDto } from './dto/update-match.dto';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RolesUser } from 'src/common/enums/roles-user.enum';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('matches')
 export class MatchesController {
   constructor(private readonly matchesService: MatchesService) {}
+
+  // -------------------------
+  //  GET BY USER
+  // -------------------------
+  @ApiOperation({
+    summary: 'Obtener partidos del equipo al que el usuario pertenece',
+  })
+  @ApiOkResponse({ description: 'Retorna los partidos' })
+  @ApiNotFoundResponse({
+    description: 'No se encontraron partidos para este equipo',
+  })
   @Get('me')
   async findByUser(@Req() req: Request) {
     const user = req.user;
     return await this.matchesService.findByUser(user);
   }
 
+  // -------------------------
+  //  FIND ALL
+  // -------------------------
+  @ApiOperation({ summary: 'Obtener todos los partidos' })
+  @ApiOkResponse({ description: 'Retorna los datos de los partidos' })
+  @ApiNotFoundResponse({ description: 'No se encontraron partidos' })
   @Get()
   async findAll() {
     return await this.matchesService.findAll();
   }
 
+  // -------------------------
+  //  GET BY ID
+  // -------------------------
+  @ApiOperation({
+    summary: 'Obtener datos del partido con identidicador solicitado',
+  })
+  @ApiOkResponse({ description: 'Retorna los datos del partido' })
+  @ApiNotFoundResponse({ description: 'Partido no encontrado' })
   @Get(':id')
   async findById(@Param('id', ParseIntPipe) id: number) {
     return await this.matchesService.findById(id);
   }
 
+  // -------------------------
+  //  CREATE
+  // -------------------------
+  @ApiOperation({
+    summary: 'Crear partido',
+  })
+  @ApiOkResponse({ description: 'Retorna los datos del partido creado' })
+  @ApiBadRequestResponse({
+    description: 'No puedes crear un partido contra ti',
+  })
+  @ApiConflictResponse({
+    description: 'Ya existe un partido con esa hora y cancha',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error al crear o actualizar el partido',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Post()
@@ -46,6 +96,20 @@ export class MatchesController {
     return await this.matchesService.create(user, dto);
   }
 
+  // -------------------------
+  //  UPDATE
+  // -------------------------
+  @ApiOperation({
+    summary: 'Actualizar partido',
+  })
+  @ApiOkResponse({ description: 'Retorna los datos de partido actualizado' })
+  @ApiUnauthorizedResponse({
+    description: 'No tienes permisos para realizar esta acción',
+  })
+  @ApiBadRequestResponse({ description: 'Error al actualizar al información' })
+  @ApiInternalServerErrorResponse({
+    description: 'Error al crear o actualizar el partido',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Put(':id')
@@ -58,6 +122,15 @@ export class MatchesController {
     return await this.matchesService.update(user, id, dto);
   }
 
+  // -------------------------
+  //  CHANGE RESULT
+  // -------------------------
+  @ApiOperation({ summary: 'Cargar resultado, solo capitan local del partido' })
+  @ApiOkResponse({ description: 'Resultado actualizado' })
+  @ApiBadRequestResponse({ description: 'No se puede cambiar el resultado' })
+  @ApiUnauthorizedResponse({
+    description: 'No puedes cambiar el resultado del partido',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Put('result/:id')
@@ -70,6 +143,16 @@ export class MatchesController {
     return await this.matchesService.changeResult(user, id, dto);
   }
 
+  // -------------------------
+  //  CONFIRM RESULT
+  // -------------------------
+  @ApiOperation({
+    summary: 'Confirmar resultado, solo capitan visitante del partido',
+  })
+  @ApiOkResponse({ description: 'Resultado confirmado' })
+  @ApiUnauthorizedResponse({
+    description: 'No tienes autorización para realizar la acción',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Patch('result/confirm/:id')
@@ -81,6 +164,20 @@ export class MatchesController {
     return await this.matchesService.confirmResult(user, id);
   }
 
+  // -------------------------
+  //  LEAVE MATCH
+  // -------------------------
+  @ApiOperation({
+    summary:
+      'Salir de un partido, solo podra hacerlo el capitan del equipo visitante',
+  })
+  @ApiOkResponse({ description: 'Has salido del partido correctamente' })
+  @ApiInternalServerErrorResponse({
+    description: 'Registro de equipos visitante no encontrado',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'No tienes permisos para ejecutar la acción',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Patch('leave/:id')
@@ -89,6 +186,15 @@ export class MatchesController {
     return await this.matchesService.leaveMatch(user, id);
   }
 
+  // -------------------------
+  //  JOIN MATCH
+  // -------------------------
+  @ApiOperation({ summary: 'Unirte a un partido' })
+  @ApiOkResponse({ description: 'Te uniste al partido correctamente' })
+  @ApiBadRequestResponse({
+    description:
+      'El partido ya tiene dos equipos o el equipo ya está participando en el partido',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Patch('join/:id')
@@ -97,6 +203,14 @@ export class MatchesController {
     return await this.matchesService.joinMatch(user, id);
   }
 
+  // -------------------------
+  //  REMOVE
+  // -------------------------
+  @ApiOperation({ summary: 'Eliminar partido, solo podra hacerlo el creado' })
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse({
+    description: 'No tienes permiso para realizar esta acción',
+  })
   @UseGuards(RolesGuard)
   @Roles(RolesUser.capitan)
   @Delete(':id')
